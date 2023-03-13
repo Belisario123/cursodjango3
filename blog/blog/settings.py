a8f8ecdd222a2fc32d1919419919d6c9f16faf03
Belisario123 <edubelinicolau85@gmail.com>
2023-03-13 17:43:29 -0300

    Configurando static_root

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -90,10 +90,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-MEDIA_URL = '/media/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
